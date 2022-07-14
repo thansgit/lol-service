243594c0491975286cb52efc1bfdbc2fbd915393
Timo Hanski <timo.hanski@gmail.com>
2022-07-14 15:01:40 +0300

    Cors to baseurl

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,7 +1,7 @@
-export const baseURL = 'https://cors-anywhere.herokuapp.com/https://some-blogapp.herokuapp.com';
+export const baseURL = 'https://cors-applied.herokuapp.com/https://some-blogapp.herokuapp.com';
 
 
 //Development
 //http://localhost:5000
 //Production
-//'https://cors-anywhere.herokuapp.com/https://some-blogapp.herokuapp.com'
+//'https://cors-applied.herokuapp.com/https://some-blogapp.herokuapp.com'
