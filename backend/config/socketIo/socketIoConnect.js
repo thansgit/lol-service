cc414e86bb7408b502c4b81731ce4e40cf7421bd
Timo Hanski <timo.hanski@gmail.com>
2022-07-24 10:18:09 +0300

    Added docker, started chat implementation with socket.io

--- backend/config/socketIo/socketIoConnect.js
+++ backend/config/socketIo/socketIoConnect.js
@@ -16,6 +16,16 @@ const socketIoConnect = (server) => {
     io.on("connection", (socket) => {
         console.log(`User connectd: ${socket.id}`);
 
+        socket.on('join_room', (data) => {
+            socket.join(data);
+            console.log(`User with ID: ${socket.id} joined room: ${data}`)
+        });
+
+        socket.on('send_message', (data) => {
+            console.log(data);
+            socket.to(data.room).emit('receive_message', data);
+        });
+
         socket.on('disconnect', () => {
             console.log('User disconnected: ', socket.id);
         });
--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import io from "socket.io-client"
 import { useDispatch, useSelector } from "react-redux";
+import Footer from "../../utils/Footer";
+import ChatBox from "./ChatBox";
 
 const socket = io('http://localhost:5000')
 const Chat = () => {
@@ -8,21 +10,30 @@ const Chat = () => {
     //console.log(socket)
 
     const users = useSelector(state => state.users)
-    const { userAuth } = users
+    const { userAuth } = users;
+
+    const [room, setRoom] = useState("");
+
+    const joinRoom = () => {
+        if (room !== "") {
+            socket.emit("join_room", room);
+        }
+    };
 
     console.log(userAuth._id);
     return (
         <>
             <div className="min-h-screen flex flex-col items-center justify-center  bg-custom-gray py-12 px-4 sm:px-6 lg:px-8">
                 <h3 className="text-white">Join a chat</h3>
-                <form>
-                    <input type='text' placeholder="John..." className="border-2 border-custom-yellow" />
-                    <input type='text' placeholder="Room ID..." className="border-2 border-custom-yellow" />
-                    <button type='submit' className="group relative w-full flex justify-center py-2 px-4
+
+                    <input type='text' placeholder={userAuth?.nickName} className="border-2 border-custom-yellow" />
+                    <input type='text' placeholder="Room ID..." onChange={(event) => setRoom(event.target.value)} className="border-2 border-custom-yellow" />
+                    <button onClick={joinRoom} className="group relative w-full flex justify-center py-2 px-4
                       text-sm font-medium rounded-md text-white
                       bg-custom-blue hover:bg-indigo-700" >Join a room</button>
-                </form>
             </div>
+            <ChatBox socket={socket} nickName={userAuth?.nickName} room={room} />
+            <Footer />
         </>
     )
 }
